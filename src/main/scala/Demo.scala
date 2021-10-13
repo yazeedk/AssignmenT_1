9edd52ff62a0d591339fad5235e41a7810b137e5
yazeedk <36983323+yazeedk@users.noreply.github.com>
2021-10-14 00:21:58 +0300

    Update Demo.scala

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -2,8 +2,8 @@ import scala.io.Source
 
 object Demo {
   def main(args: Array[String]) {
-    val fpath = "C:\\spark-3.1.2-bin-hadoop3.2\\elonmusk_tweets.csv"
-    val fSource = Source.fromFile(fpath)
+    val firstpath = "C:\\spark-3.1.2-bin-hadoop3.2\\elonmusk_tweets.csv"
+    val firatSource = Source.fromFile(fpath)
     println {
       "Reading File:"
     }
